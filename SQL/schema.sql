c62cbf0b6c559b0e13a84f7b443619e2c0d85620
Hans Shen <116024825+cameragrammar@users.noreply.github.com>
2023-03-16 23:10:18 -0600

    Add new schema file

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -3,25 +3,25 @@ CREATE database employee_DB;
 
 USE employee_DB;
 
-CREATE TABLE dept (
-    id INT AUTO_INCREMENT NOT NULL,
-    dept_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 0) NOT NULL,
-    dept_id INT,
-    PRIMARY KEY (id)
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    PRIMARY KEY (id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
