9b64de801b6a297835733a0d809a50819d96538a
Hans Shen <116024825+cameragrammar@users.noreply.github.com>
2023-03-16 23:32:10 -0600

    New seeds file

--- SQL/seeds.sql
+++ SQL/seeds.sql
@@ -13,26 +13,26 @@ VALUES ('Dennis', 'Lindsey', 5, 2);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Daniel', 'Ainge', 6, null);
 
-INSERT INTO dept (dept_name)
+INSERT INTO department (department_name)
 VALUES ('Coaching');
-INSERT INTO dept (dept_name)
+INSERT INTO department (department_name)
 VALUES ('Basketball Operations');
-INSERT INTO dept (dept_name)
+INSERT INTO department (department_name)
 VALUES ('Scouting');
-INSERT INTO dept (dept_name)
+INSERT INTO department (department_name)
 VALUES ('Human Resources');
-INSERT INTO dept (dept_name)
+INSERT INTO department (department_name)
 VALUES ('Executive Leadership');
 
-INSERT INTO role (title, salary, dept_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ('GM', 120000, 1);
-INSERT INTO role (title, salary, dept_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ('Coaching', 100000, 2);
-INSERT INTO role (title, salary, dept_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ('President Basketball', 110000, 4);
-INSERT INTO role (title, salary, dept_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ('Assistant Coaching', 80000, 3);
-INSERT INTO role (title, salary, dept_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ('Head Scout', 75000, 5);
-INSERT INTO role (title, salary, dept_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ('CEO', 250000, 6);
